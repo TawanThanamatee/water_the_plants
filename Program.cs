5345d353016b6df1d2082183cb42b7d0f3dfc537
LAPTOP-1M8RSRLB\Pai <tawan.th@mail.kmutt.ac.th>
2021-06-02 23:59:35 +0700

    ส่งการบ้าน

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace water_the_plants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //There are five trees in one row.
+            Console.Write("Tree_row : ");
+            int tree = int.Parse(Console.ReadLine());
+            Console.Write("Time : ");
+            double time = double.Parse(Console.ReadLine());
+            Console.Write("Rain : ");
+            bool rain = bool.Parse(Console.ReadLine());
+
+            if (time == 8.00 && rain != true || time == 16.00 && rain != true)
+            {
+                for (int water_the_plants_row = 0; water_the_plants_row < tree; water_the_plants_row++)
+                {
+                    for (int water_the_plants_collum = 0; water_the_plants_collum < 5; water_the_plants_collum++)
+                    {
+                        Console.Write(" ");
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else if(rain == true)
+            {
+                Console.WriteLine("it's raining.");
+            }
+            else if(time != 8.00 || time != 16.00)
+            {
+                Console.WriteLine("Not time.");
+            } 
+        }
+    }
+}
